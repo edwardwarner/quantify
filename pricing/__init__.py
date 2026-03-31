e0c45b233790c2ca6f2d53ab29670ca6ff4743c0
Edward Warner <edwarner88@gmail.com>
2026-03-31 23:48:01 +1000

    add: basic dcf model

--- pricing/__init__.py
+++ pricing/__init__.py
@@ -1,9 +1,10 @@
 from .black_scholes import BlackScholes
-
 from .monte_carlo import MonteCarlo
+from .dcf import DCF
 
 
 __all__ = [
     "BlackScholes",
-    "MonteCarlo"
+    "MonteCarlo",
+    "DCF"
 ]
