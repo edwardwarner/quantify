9b6a39e63bb82ab5adf783d9d205802a60f98ee4
Edward Warner <edwarner88@gmail.com>
2026-03-31 23:47:52 +1000

    add: bond instruments

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -1,11 +1,17 @@
 from .base import Instrument
 
+from .bond import (
+    ZeroCouponBond,
+    CouponBond
+)
+
 from .option import (
     EuropeanOption
 )
 
 __all__ = [
     "Instrument",
-
-    "EuropeanOption"
+    "EuropeanOption",
+    "ZeroCouponBond",
+    "CouponBond"
 ]
